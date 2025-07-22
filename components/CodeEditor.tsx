cddb11315a8feee689e70417d5f9b560b5322aad
Erich Petrocelli <105027683+erich0101@users.noreply.github.com>
2025-07-22 20:24:50 -0300

    Update CodeEditor.tsx

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,10 +1,11 @@
-
 import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { json } from '@codemirror/lang-json';
 import { okaidia } from '@uiw/codemirror-theme-okaidia';
 import { EditorView } from '@codemirror/view';
+import { variableHighlighter } from '../services/variableHighlighter';
+import { EditorState } from '@codemirror/state';
 
 interface CodeEditorProps {
     value: string;
@@ -12,9 +13,11 @@ interface CodeEditorProps {
     language: 'json' | 'javascript' | 'text';
     readOnly?: boolean;
     wrapLines?: boolean;
+    activeVariables?: string[];
+    singleLine?: boolean;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange, language, readOnly = false, wrapLines = false }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange, language, readOnly = false, wrapLines = false, activeVariables, singleLine = false }) => {
     const extensions = [];
     if (language === 'javascript') {
         extensions.push(javascript({ jsx: true, typescript: true }));
@@ -25,8 +28,17 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange, language, read
     if (wrapLines) {
         extensions.push(EditorView.lineWrapping);
     }
+    
+    if (activeVariables) {
+        extensions.push(variableHighlighter(activeVariables));
+    }
 
-    return (
+    if (singleLine) {
+        // This makes it behave like a single line input by preventing new lines
+        extensions.push(EditorState.transactionFilter.of(tr => tr.newDoc.lines > 1 ? [] : [tr]));
+    }
+    
+    const editor = (
         <CodeMirror
             value={value}
             height="100%"
@@ -36,8 +48,22 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange, language, read
             readOnly={readOnly}
             className="h-full"
             style={{height: '100%'}}
+            basicSetup={{
+                lineNumbers: !singleLine,
+                foldGutter: !singleLine,
+                highlightActiveLine: !singleLine,
+                highlightActiveLineGutter: !singleLine,
+                autocompletion: !singleLine,
+                searchKeymap: !singleLine,
+            }}
         />
     );
+    
+    if (singleLine) {
+        return <div className="url-editor-wrapper">{editor}</div>
+    }
+    
+    return editor;
 };
 
 export default CodeEditor;
